5c994d2480ecbb60cd893ed00da6e033e11f787f
Robert Torres <robertt224@gmail.com>
2025-04-24 21:45:49 -0400

    Update MainActivity to observe and apply theme preferences

--- androidApp/src/main/java/com/cocktailcraft/MainActivity.kt
+++ androidApp/src/main/java/com/cocktailcraft/MainActivity.kt
@@ -6,15 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.cocktailcraft.ui.main.MainScreen
 import com.cocktailcraft.ui.theme.CocktailBarTheme
+import com.cocktailcraft.viewmodel.ThemeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CocktailBarTheme {
+            // Get the ThemeViewModel to observe dark mode preference
+            val themeViewModel: ThemeViewModel = viewModel()
+            val isDarkMode by themeViewModel.isDarkMode.collectAsState()
+
+            CocktailBarTheme(darkTheme = isDarkMode) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
