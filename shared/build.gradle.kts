a318d3141514ae1bc5be60b0547615c59fb3f2c7
Robert Torres <robertt224@gmail.com>
2025-05-25 20:25:53 -0600

    Fix Kotlin plugin version conflict
    - Update root build.gradle.kts to use Kotlin 1.9.22
    - Remove version declarations from shared/build.gradle.kts to inherit from root
    - Resolves InvalidPluginRequestException for conflicting plugin versions

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
     id("com.android.library")
 }
 
